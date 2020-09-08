34fa9e77c858e3fda359dfc2712487e52863878d
Wenyi_Wang <Wenyi_Wang@W10D94RNH2>
2020-09-08 00:27:05 -0500

    initial setup

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                (c)2020 Travel Planner. All Rights Reserved. Website Made by Travel Planner team.
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <p className="title">
+                    Travel Planner
+                </p>
+            </header>
+        );
+    }
+}
+export default Header;
