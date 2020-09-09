073182e9592a81a598c0e53ca4422edd089dc057
Zoe Meng <zoebo1225@gmail.com>
2020-09-09 15:08:22 -0700

    midified src/App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
+import Travel_planner_logo from './assets/images/travel_planner_logo.svg';
+import User_icon from './assets/images/user.svg';
+import {Layout, Row, Col} from 'antd';
+
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Layout>
+      <Header className = "home-header">
+        <Row className = "row-class">
+          <Col span= {12}><img src= {Travel_planner_logo} className= "app-logo" alt="logo"/></Col>
+          <Col span= {12} className = "id-class">
+            <span>Sign in</span>
+            <img src= {User_icon} className= "user-icon" alt="user"/>
+          </Col>
+        </Row>
+      </Header>
+      <Content>
+        <Main />
+      </Content>
+    </Layout>
   );
 }
 
