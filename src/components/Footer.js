f822c9b9c4b23e27a800dcd1a8bc99e8a0cd356b
Wenyi_Wang <Wenyi_Wang@W10D94RNH2>
2020-09-08 19:55:42 -0500

    test from upstream

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-                (c)2020 Travel Planner Test Github. All Rights Reserved. Website Made by Travel Planner team.
+                (c)2020 Travel Planner Test Github. All Rights Reserved. Website Made by Travel Planner team. Test from upstream.
             </footer>
         );
     }
