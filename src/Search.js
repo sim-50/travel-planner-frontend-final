c7176dc8124f0e29a9234484eb629183212e5c06
Chopardissmo <gel@alumni.usc.edu>
2020-09-10 23:40:22 -0500

    initial befor adding search panel

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
-
-function App() {
+function Search() {
   return (
-    <div className="App">
+    <div className="searchPage">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Search;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,19 @@
-import React from 'react';
-// import HomeBg from '../assets/images/home_pic.jpeg';
-import {Row, Col, Input} from 'antd';
+import React, { Component } from 'react';
+import MapContainer from './MapContainer';
 
+class Main extends Component {
 
-const  {Search} = Input;
-
-const Main = () => {
-  // function write here...
-
-
-return(
-    <Row className = "home-body">
-      <Col span={24} className = "home-bg-col">
-        <Row className = "home-detail">
-          <Col span={8}></Col>
-          <Col span={8} className = "home-detail-child">
-              <p className = "bg-title">Hi, Let plan your next road trip efficiently! </p>
-            <Search
-              className = "home-search_bar"
-              placeholder="Where do you want to go?"
-              onSearch={value => console.log(value)}
-              // style={{ width: 200 }}
-            />
-          </Col>
-          <Col span={8}></Col>
-        </Row>
-      </Col>
-    </Row>
- 
-  )
+    render() {
+        return (
+            <div className='main'>
+                <div className="left-side">
+                    Search Panel
+                </div>
+                <div className="right-side">
+                    <MapContainer />
+                </div>
+            </div>
+        );
+    }
 }
-
-
 export default Main;
