8fcea6ec0d7a7739477fd907fe6c9e002756f9e2
include-steve-kx <ronin2017@g.ucla.edu>
2020-09-27 12:15:38 -0700

    1 change

--- src/components/ResultDisplayPanel.js
+++ src/components/ResultDisplayPanel.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import ImgContainer from './ImgContainer';
 import LocationOptionList from './LocationOptionList';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
-import {Button} from 'antd';
-=======
 import {Button, Typography} from 'antd';
->>>>>>> upstream/dev
 import RecommendPlanList from './RecommendPlanList';
 import TravelSchedulePanel from "./TravelSchedulePanel";
 import "../styles/SearchResult.css";
@@ -54,24 +50,15 @@ class ResultDisplayPanel extends Component {
                         <Switch>
                             <Route exact path={`/searchResult/${cityName}`}>
                             {this.state.showRecommendButton &&
-<<<<<<< HEAD
-                                <Button className="recommend-button"type="primary" block onClick = {this.clickRecommendButton}>
-                                    Have no idea about the following places? Click here to get inspiration!
-                                </Button>
-=======
                                 <div className="recommend-div">
                                     Have no idea about the following places? <Text onClick = {this.clickRecommendButton} underline> Click here</Text> to get inspiration!
                                 </div>
->>>>>>> upstream/dev
                             }
                             {this.state.showPlanList && 
                             <RecommendPlanList
                                 showOnMap={this.props.showOnMap}
                                 planList={this.props.planList}
-<<<<<<< HEAD
-=======
                                // recommendPlanList = {this.props.recommendationPlanList}
->>>>>>> upstream/dev
                             />}
                             {this.state.showLocationList &&
                                 <LocationOptionList
@@ -109,61 +96,4 @@ ResultDisplayPanel.propTypes = {
     allTypes: PropTypes.array.isRequired,
 };
 
-<<<<<<< HEAD
 export default ResultDisplayPanel;
-
-// import React, { Component } from "react";
-// import ImgContainer from "./ImgContainer";
-// import LocationOptionList from "./LocationOptionList";
-// import TravelSchedulePanel from "./TravelSchedulePanel";
-// import "../styles/SearchResult.css";
-// import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
-// import PropTypes from "prop-types";
-// import history from "../history";
-
-// class ResultDisplayPanel extends Component {
-//     render() {
-//         const { cityName, cityImg, citySearchResult, selectedList, allTypes } = this.props;
-
-//         return (
-//             <BrowserRouter>
-//                 <Router history={history}>
-//                     <div className="container">
-//                         <ImgContainer cityImg={cityImg} />
-//                         <Switch>
-//                             <Route exact path={`/searchResult/${cityName}`}>
-//                                 <LocationOptionList
-//                                     updateSelectedLocation={this.props.updateSelectedLocation}
-//                                     selectedList={selectedList}
-//                                     citySearchResult={citySearchResult}
-//                                     allTypes={allTypes}
-//                                     filterByName={this.props.filterByName}
-//                                     filterByType={this.props.filterByType}
-//                                     switchToTravelSchedulePanel={this.props.switchToTravelSchedulePanel}
-//                                     updateWaypoints={this.props.updateWaypoints}
-//                                 />
-//                             </Route>
-
-//                             <Route path={`/searchResult/${cityName}/travelSchedule`} >
-//                                 <TravelSchedulePanel 
-//                                     selectedList={selectedList}
-//                                 />
-//                             </Route>
-//                         </Switch>
-//                     </div>
-//                 </Router>
-//             </BrowserRouter>
-//         );
-//     }
-// }
-
-// ResultDisplayPanel.propTypes = {
-//     citySearchResult: PropTypes.array.isRequired,
-//     cityImg: PropTypes.string.isRequired,
-//     allTypes: PropTypes.array.isRequired,
-// };
-
-// export default ResultDisplayPanel;
-=======
-export default ResultDisplayPanel;
->>>>>>> upstream/dev
