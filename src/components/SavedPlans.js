d7830d4d29d58c9e48b690577d4b1ae1cb2f930b
include-steve-kx <ronin2017@g.ucla.edu>
2020-10-03 17:22:36 -0700

    saved plans modification

--- src/components/SavedPlans.js
+++ src/components/SavedPlans.js
@@ -197,6 +197,7 @@ class SavedPlans extends Component {
             <Button onClick={() => {
               this.setModalVisible(true);
               this.setPlanDetail(record.planDetail);
+              console.log(record.planDetail);
               this.setPlanName(record.name);
             }}>
               Details
